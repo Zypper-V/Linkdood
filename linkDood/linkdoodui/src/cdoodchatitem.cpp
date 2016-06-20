1406ac2e52e3198e8cb8daed4f4ee9408c3ad8da
liang_zhang_dev <zhangliang@syberos.com>
2016-06-20 15:33:07 +0800

    添加会话item
    [提交原因]
    新功能
    [详细描述]
    添加item,用于UI显示
    [Bug ID]
    [Bug 原因]
    [解决方法]
    [其它]

--- linkDood/linkdoodui/src/cdoodchatitem.cpp
+++ linkDood/linkdoodui/src/cdoodchatitem.cpp
@@ -4,3 +4,169 @@ CDoodChatItem::CDoodChatItem(QObject *parent) : QObject(parent)
 {
 
 }
+
+QString CDoodChatItem::msgType() const
+{
+    return mMsgYype;
+}
+
+QString CDoodChatItem::setMsgType(const QString &data)
+{
+    if(mMsgYype == data) {
+        return data;
+    }
+    mMsgYype = data;
+    emit msgTypeChanged();
+    return mMsgYype;
+}
+
+QString CDoodChatItem::activeType() const
+{
+    return mActiveType;
+}
+
+QString CDoodChatItem::setActiveType(const QString &data)
+{
+    if(mActiveType == data) {
+        return data;
+    }
+    mActiveType = data;
+    emit activeTypeChanged();
+    return mActiveType;
+}
+
+QString CDoodChatItem::msgId() const
+{
+    return mMsgId;
+}
+
+QString CDoodChatItem::setMsgId(const QString &data)
+{
+    if(mMsgId == data) {
+        return data;
+    }
+    mMsgId = data;
+    emit msgIdChanged();
+    return mMsgId;
+}
+
+QString CDoodChatItem::targetId() const
+{
+    return mTargetId;
+}
+
+QString CDoodChatItem::setTargetId(const QString &data)
+{
+    if(mTargetId == data) {
+        return data;
+    }
+    mTargetId = data;
+    emit targetIdChanged();
+    return mTargetId;
+}
+
+QString CDoodChatItem::fromId() const
+{
+    return mFromId;
+}
+
+QString CDoodChatItem::setFromId(const QString &data)
+{
+    if(mFromId == data) {
+        return data;
+    }
+    mFromId = data;
+    emit fromIdChanged();
+    return mFromId;
+}
+
+QString CDoodChatItem::toId() const
+{
+    return mToId;
+}
+
+QString CDoodChatItem::setToId(const QString &data)
+{
+    if(mToId == data) {
+        return data;
+    }
+    mToId = data;
+    emit toIdChanged();
+    return mToId;
+}
+
+QString CDoodChatItem::localId() const
+{
+    return mLocalId;
+}
+
+QString CDoodChatItem::setLocalId(const QString &data)
+{
+    if(mLocalId == data) {
+        return data;
+    }
+    mLocalId = data;
+    emit localIdChanged();
+    return mLocalId;
+}
+
+QString CDoodChatItem::relatedMsgId() const
+{
+    return mRelatedMsgId;
+}
+
+QString CDoodChatItem::setRelatedMsgId(const QString &data)
+{
+    if(mRelatedMsgId == data) {
+        return data;
+    }
+    mRelatedMsgId = data;
+    emit relatedMsgIdChanged();
+    return mRelatedMsgId;
+}
+
+QString CDoodChatItem::time() const
+{
+    return mTime;
+}
+
+QString CDoodChatItem::setTime(const QString &data)
+{
+    if(mTime == data) {
+        return data;
+    }
+    mTime = data;
+    emit timeChanged();
+    return mTime;
+}
+
+QString CDoodChatItem::body() const
+{
+    return mBody;
+}
+
+QString CDoodChatItem::setBody(const QString &data)
+{
+    if(mBody == data) {
+        return data;
+    }
+    mBody = data;
+    emit bodyChanged();
+    return mBody;
+}
+
+QString CDoodChatItem::sgProperties() const
+{
+    return msgProperties;
+}
+
+QString CDoodChatItem::setSgProperties(const QString &data)
+{
+    if(msgProperties == data) {
+        return data;
+    }
+    msgProperties = data;
+    emit sgPropertiesChanged();
+    return msgProperties;
+}
+
--- linkDood/linkdoodui/src/cdoodchatitem.h
+++ linkDood/linkdoodui/src/cdoodchatitem.h
@@ -7,22 +7,78 @@
 class CDoodChatItem : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString msgType READ msgType WRITE setMsgType NOTIFY msgTypeChanged)
+    Q_PROPERTY(QString activeType READ activeType WRITE setActiveType NOTIFY activeTypeChanged)
+    Q_PROPERTY(QString msgId READ msgId WRITE setMsgId NOTIFY msgIdChanged)
+    Q_PROPERTY(QString targetId READ targetId WRITE setTargetId NOTIFY targetIdChanged)
+    Q_PROPERTY(QString fromId READ fromId WRITE setFromId NOTIFY fromIdChanged)
+    Q_PROPERTY(QString toId READ toId WRITE setToId NOTIFY toIdChanged)
+    Q_PROPERTY(QString localId READ localId WRITE setLocalId NOTIFY localIdChanged)
+    Q_PROPERTY(QString relatedMsgId READ relatedMsgId WRITE setRelatedMsgId NOTIFY relatedMsgIdChanged)
+    Q_PROPERTY(QString time READ time WRITE setTime NOTIFY timeChanged)
+    Q_PROPERTY(QString body READ body WRITE setBody NOTIFY bodyChanged)
+    Q_PROPERTY(QString sgProperties READ sgProperties WRITE setSgProperties NOTIFY sgPropertiesChanged)
 
 public:
+
     explicit CDoodChatItem(QObject *parent = 0);
 
-signals:
+    QString msgType() const;
+    QString setMsgType(const QString &data);
 
-public slots:
+    QString activeType() const;
+    QString setActiveType(const QString &data);
+
+    QString msgId() const;
+    QString setMsgId(const QString &data);
+
+    QString targetId() const;
+    QString setTargetId(const QString &data);
+
+    QString fromId() const;
+    QString setFromId(const QString &data);
+
+    QString toId() const;
+    QString setToId(const QString &data);
+
+    QString localId() const;
+    QString setLocalId(const QString &data);
+
+    QString relatedMsgId() const;
+    QString setRelatedMsgId(const QString &data);
+
+    QString time() const;
+    QString setTime(const QString &data);
+
+    QString body() const;
+    QString setBody(const QString &data);
+
+    QString sgProperties() const;
+    QString setSgProperties(const QString &data);
+
+
+signals:
+    void msgTypeChanged();
+    void activeTypeChanged();
+    void msgIdChanged();
+    void targetIdChanged();
+    void fromIdChanged();
+    void toIdChanged();
+    void localIdChanged();
+    void relatedMsgIdChanged();
+    void timeChanged();
+    void bodyChanged();
+    void sgPropertiesChanged();
+
 private:
-    QString mMsgtype;// 消息类型
+    QString mMsgYype;// 消息类型
     QString mActiveType;// 消息事件属性 1. 阅后即焚 2 有问必答 3 活动
-    QString mMsgid;// 消息 ID
-    QString mTargetid; // 会话者 ID (群或用户等)
-    QString mFromid;// 接收者 ID
-    QString mToid;// 发送者 ID
-    QString mLocalid; // 本地 ID
-    QString mRelatedMsgid; // 关联消息 ID
+    QString mMsgId;// 消息 ID
+    QString mTargetId; // 会话者 ID (群或用户等)
+    QString mFromId;// 接收者 ID
+    QString mToId;// 发送者 ID
+    QString mLocalId; // 本地 ID
+    QString mRelatedMsgId; // 关联消息 ID
     QString mTime;// 发送时间
     QString mBody;// 消息内容
     QString msgProperties;
